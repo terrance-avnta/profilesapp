84f485865348ef90e8f79ce424bce6bf4ae9ad9c
Terrance Li <terrance@auventa.com>
2025-03-14 13:22:42 -0400

    displaying user profile

---
+++ amplify/auth/post-confirmation/resource.ts
@@ -0,0 +1,36 @@
+import { defineAuth } from '@aws-amplify/backend';
+import { postConfirmation } from './post-confirmation/resource';
+
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  triggers: {
+    postConfirmation
+  }
+});
+
+// Define schema
+const schema = a
+  .schema({
+    UserProfile: a
+      .model({
+        email: a.string(),
+        profileOwner: a.string(),
+      })
+      .authorization((allow) => [allow.ownerDefinedIn("profileOwner")]),
+  })
+  .authorization((allow) => [allow.resource(postConfirmation)]);
+
+export type Schema = ClientSchema<typeof schema>;
+
+// Define data and authorization modes
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
